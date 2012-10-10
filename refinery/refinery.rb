052f968fb8e1986677d964f20bf6b30e0179f5c3
Mir <mirosul@gmail.com>
2012-10-10 13:38:15 +0300

    refinery - pseudocode

---
+++ refinery/refinery.rb
@@ -0,0 +1,20 @@
+
+
+# reads an input file from ./tracks
+
+
+# calculate the distance between the points
+
+
+# calculate total distance (from the point 0) Td
+
+
+# build an array with two columns: Td, Elevation
+
+
+# interpolate points at a given interval
+
+
+# output the new array (Td, elevation, slope)
+
+
