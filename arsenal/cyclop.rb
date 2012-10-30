60c3be1dcc7343be80643184f5987261aa978db4
Mir <mirosul@gmail.com>
2012-10-30 04:19:49 +0200

    basic cyclop gui

--- arsenal/cyclop.rb
+++ arsenal/cyclop.rb
@@ -3,95 +3,34 @@
 require 'zmq'
 require 'gtk2'
 
-BIND_TO = 'tcp://127.0.0.1:5556'
+CONTROL_QUEUE = 'tcp://127.0.0.1:5556'
 TICK_DISTANCE = 559 # mm
 SPEED_TICK_RESOLUTION = 5 # speed measured between last 5 ticks
 
-class RubyApp < Gtk::Window
-  @@subscriber
+class Cyclop < Gtk::Window
+  @@control_queue
+  @@distance_ctl
+  @@tracks_ctl
 
   def initialize
     super
-
-    set_title "Center"
-    signal_connect "destroy" do
-      destroy_zmq
-      Gtk.main_quit
-    end
-
     init_ui
-    init_variables
     init_zmq
-    init_timer
     show_all
   end
 
   def init_zmq
-    set_status "Connecting to tick-factory..."
     ctx = ZMQ::Context.new(1)
-    @@subscriber = ctx.socket(ZMQ::SUB);
-    @@subscriber.setsockopt(ZMQ::SUBSCRIBE, "")
-    @@subscriber.connect(BIND_TO);
-    set_status "Connected."
+    @@control_queue = ctx.socket(ZMQ::REQ);
+    @@control_queue.connect(CONTROL_QUEUE);
   end
 
   def destroy_zmq
-    @@subscriber.setsockopt(ZMQ::UNSUBSCRIBE, "")
-    @@subscriber.close
-  end
-
-  def init_timer
-    Gtk.timeout_add(1000) do
-      @@selected_track = 'solaison-part'
-      msg = '00000000'
-      msg = @@subscriber.recv(0)
-
-      current_time = Time.now
-      current_speed =  (1.0 * TICK_DISTANCE / 1000000.0 * SPEED_TICK_RESOLUTION) / ((current_time - @@speed_initial_time_minus_5).to_f / 3600.0)
-      average_speed =  (1.0 * @@distance / 1000000.0) / ((current_time - @@initial_time).to_f / 3600.0)
-
-      @@distance = @@distance + TICK_DISTANCE
-      @@distance_ctl.text = "Total distance: #{"%9.2f" % (@@distance/1000.0)} m"
-      # set_status "speed: #{"%8.2f" % current_speed} km/h, average speed: #{"%8.2f" % average_speed} km/h"
-
-      img = @@distance / 2000 * 2
-      image_file = "tracks/#{@@selected_track}/svcache/sv-#{img.to_s.rjust(8, '0')}.jpg"
-      set_status image_file
-      @@image_ctl.set(image_file)
-      @@image_ctl.queue_draw
-
-      # TODO - shift right array
-      @@speed_initial_time_minus_5 = @@speed_initial_time_minus_4
-      @@speed_initial_time_minus_4 = @@speed_initial_time_minus_3
-      @@speed_initial_time_minus_3 = @@speed_initial_time_minus_2
-      @@speed_initial_time_minus_2 = @@speed_initial_time_minus_1
-      @@speed_initial_time_minus_1 = current_time
-    end
+    @@control_queue.close
   end
 
   def set_status status
-    @@status.buffer.text = @@status.buffer.text + "\n" + status
-  end
-
-  # def start_button_click(sender)
-  def init_variables
-    # select existing track
-    @@selected_track = @@tracks_ctl.active_text
-    set_status @@selected_track
-    csv_file = "./tracks/#{@@selected_track}/#{@@selected_track}_smooth.csv"
-
-    @@distance = 0 # mm
-    @@initial_time = Time.now
-    @@speed_ticks = 0
-
-    # TODO - array
-    @@speed_initial_time_minus_5 = 0
-    @@speed_initial_time_minus_4 = 0
-    @@speed_initial_time_minus_3 = 0
-    @@speed_initial_time_minus_2 = 0
-    @@speed_initial_time_minus_1 = 0
-
-    set_status "Detecting movement..."
+    @@status.buffer.text = status
   end
 
   def init_ui
@@ -99,19 +38,14 @@ class RubyApp < Gtk::Window
     @@fixed = Gtk::Fixed.new
     add @@fixed
 
-    # status label
-    @@status = Gtk::TextView.new
-    @@status.set_size_request 300, 60
-    @@fixed.put @@status, 210, 10
-
-    # distance label
-    @@distance_ctl = Gtk::Label.new
-    @@distance_ctl.set_size_request 300, 60
-    @@fixed.put @@distance_ctl, 40, 150
-
     # streetview image
     @@image_ctl = Gtk::Image.new("tracks/solaison-part/svcache/sv-00000000.jpg")
-    @@fixed.put @@image_ctl, 10, 80
+    @@fixed.put @@image_ctl, 560, 0
+
+    # status label
+    @@status = Gtk::TextView.new
+    @@status.set_size_request 560, 50
+    @@fixed.put @@status, 0, 590
 
     # available tracks
     tracks = []
@@ -122,25 +56,128 @@ class RubyApp < Gtk::Window
 
     @@tracks_ctl = Gtk::ComboBox.new
     tracks.each_with_index do |track, index|
-      @@tracks_ctl.append_text "#{track}#{ index == 0 ? ' (default)' : ''}"
+      @@tracks_ctl.append_text track
     end
     @@tracks_ctl.set_active 0
+    @@tracks_ctl.set_size_request 300, 25
     @@fixed.put @@tracks_ctl, 10, 10
 
-    # start button
-    button = Gtk::Button.new "Start reading"
-    button.set_size_request 80, 35
-    # button.signal_connect "clicked" do |sender|
-    #   start_button_click(sender)
-    # end
-    @@fixed.put button, 10, 40
+    # select track button
+    select_track_button = Gtk::Button.new "Select track"
+    select_track_button.set_size_request 120, 25
+    select_track_button.signal_connect "clicked" do |sender|
+      select_track_click(sender)
+    end
+    @@fixed.put select_track_button, 320, 10
+
+    # start track button
+    start_track_button = Gtk::Button.new "Star trek"
+    start_track_button.set_size_request 120, 25
+    start_track_button.signal_connect "clicked" do |sender|
+      start_track_click(sender)
+    end
+    @@fixed.put start_track_button, 10, 45
+
+    # start track button
+    stop_track_button = Gtk::Button.new "Stop track"
+    stop_track_button.set_size_request 120, 25
+    stop_track_button.signal_connect "clicked" do |sender|
+      stop_track_click(sender)
+    end
+    @@fixed.put stop_track_button, 140, 45
+
+    # start track button
+    status_button = Gtk::Button.new "Status"
+    status_button.set_size_request 120, 25
+    status_button.signal_connect "clicked" do |sender|
+      status_click(sender)
+    end
+    @@fixed.put status_button, 270, 45
+
+
+    ### INDICATORS
+
+    # distance label
+    @@distance_ctl = Gtk::Label.new("Distance: ")
+    @@distance_ctl.set_size_request 520, 25
+    @@distance_ctl.set_alignment 0, 0
+    @@fixed.put @@distance_ctl, 10, 80
+
+    # timer label
+    @@timer_ctl = Gtk::Label.new("Timer: ")
+    @@timer_ctl.set_size_request 520, 25
+    @@timer_ctl.set_alignment 0, 0
+    @@fixed.put @@timer_ctl, 10, 115
+
+    # current speed label
+    @@current_speed_ctl = Gtk::Label.new("Current speed: ")
+    @@current_speed_ctl.set_size_request 520, 25
+    @@current_speed_ctl.set_alignment 0, 0
+    @@fixed.put @@current_speed_ctl, 10, 150
+
+    # average speed label
+    @@average_speed_ctl = Gtk::Label.new("Average speed: ")
+    @@average_speed_ctl.set_size_request 520, 25
+    @@average_speed_ctl.set_alignment 0, 0
+    @@fixed.put @@average_speed_ctl, 10, 185
+
+
 
     # window properties
-    set_default_size 800, 800
+    set_title "Cyclop"
+    set_default_size 1200, 640
     set_window_position Gtk::Window::POS_CENTER
+
+    signal_connect "destroy" do
+      destroy_zmq
+      Gtk.main_quit
+    end
   end
+
+  def select_track_click(sender)
+    @@control_queue.send("load " + @@tracks_ctl.active_text)
+    set_status @@control_queue.recv
+  end
+
+  def start_track_click(sender)
+    @@control_queue.send("start")
+    set_status @@control_queue.recv
+  end
+
+  def stop_track_click(sender)
+    @@control_queue.send("stop")
+    set_status @@control_queue.recv
+  end
+
+  def status_click(sender)
+    # @@control_queue.send("status")
+    # set_status @@control_queue.recv
+
+    @@control_queue.send("get_distance")
+    distance = @@control_queue.recv
+    @@distance_ctl.set_markup("Distance: <b>#{("%12.2f" % (distance.to_i / 1000.0)).strip} m</b>")
+
+    @@control_queue.send("get_timer")
+    timer = @@control_queue.recv
+    @@timer_ctl.set_markup("Timer: <b>#{("%12.0f" % timer.to_i).strip} s</b>")
+
+    @@control_queue.send("get_current_speed")
+    current_speed = @@control_queue.recv
+    @@current_speed_ctl.set_markup("Current speed: <b>#{("%12.2f" % current_speed.to_f).strip} km/h</b>")
+
+    @@control_queue.send("get_average_speed")
+    average_speed = @@control_queue.recv
+    @@average_speed_ctl.set_markup("Average speed: <b>#{("%12.2f" % average_speed.to_f).strip} km/h</b>")
+
+    @@control_queue.send("get_image")
+    image_file = @@control_queue.recv
+    @@image_ctl.set(image_file)
+    @@image_ctl.queue_draw
+    set_status image_file
+  end
+
 end
 
 Gtk.init
-window = RubyApp.new
+window = Cyclop.new
 Gtk.main
